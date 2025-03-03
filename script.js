0ce7b764ab5ace8540d00f56cd4121e3b13957c1
ChuDang18 <dangluongchu@gmail.com>
2025-03-03 17:14:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function drawHeart(x, y, size) {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.bezierCurveTo(x - size, y - size, x - 2 * size, y, x, y + size);
+    ctx.bezierCurveTo(x + 2 * size, y, x + size, y - size, x, y);
+    ctx.fill();
+}
+
+function drawText(x, y) {
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial";
+    ctx.fillText("Hello!", x, y);
+}
+
+function drawConfetti(x, y) {
+    for (let i = 0; i < 20; i++) {
+        ctx.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        ctx.fillRect(x + Math.random() * 50 - 25, y + Math.random() * 50 - 25, 5, 5);
+    }
+}
+
+canvas.addEventListener("click", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    drawHeart(x, y, 20);
+    drawText(x - 30, y);
+    drawConfetti(x, y);
+});
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
